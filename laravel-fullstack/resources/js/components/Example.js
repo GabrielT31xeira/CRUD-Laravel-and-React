0a0becae599b68d7728bb4ad75e29612db70d4e9
Teixeira <gt3ixeira@gmail.com>
2021-01-15 11:06:35 -0300

    plugins and controller updates

--- laravel-fullstack/resources/js/components/Example.js
+++ laravel-fullstack/resources/js/components/Example.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './Nav';
+import Contacts from './Contacts';
+import AddContact from './addContact';
 
 function Example() {
     return (
-        <div className="container">
-            <h1>Hello Word</h1>
-        </div>
+        <Router>
+            <Nav />
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-8">
+                        <Switch>
+                            <Route path="/" exact component={Contacts} />
+                            <Route path="/addContact" exact component={AddContact} />
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+
+        </Router>
     );
 }
 
---
+++ laravel-fullstack/resources/js/components/addContact.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import axios from 'axios';
+
+import '../../css/global.css';
+
+class AddContact extends React.Component {
+    state = {
+        nome: '',
+        email:'',
+        telefone:''
+    }
+    handleInput = (e) =>{
+        this.setState({[e.target.name]: e.target.value});
+    }
+    saveContact = async (e) => {
+        e.preventDefault();
+        const res = await axios.post("/contact", this.state);
+        console.log(res);
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.saveContact}>
+                    <h1>Criar Contato</h1>
+                    <div className="form-group">
+                        <input type="text" name="nome" className="form-control" value={this.state.nome}
+                            onChange={this.handleInput} placeholder="Nome" required/>
+                        <input type="text" name="email" className="form-control" value={this.state.email}
+                            onChange={this.handleInput} placeholder="Email" required/>
+                        <input type="text" name="telefone" className="form-control" value={this.state.telefone}
+                            onChange={this.handleInput} placeholder="telefone" required/>
+                        <button type="submit" className="btn btn-primary">Adicionar</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddContact;
